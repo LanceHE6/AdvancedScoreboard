c14f18519c2d8371d539b506d9c83260f50bba37
Hycer_Lance <92719376+LanceHE6@users.noreply.github.com>
2026-04-01 03:40:24 +0000

    chore:移除无用的全局实例

--- src/main/java/cn/hycer/advancedscoreboard/Global/Global.java
+++ src/main/java/cn/hycer/advancedscoreboard/Global/Global.java
@@ -5,11 +5,10 @@ import net.minecraft.scoreboard.Scoreboard;
 import org.apache.logging.log4j.Logger;
 
 public class Global {
+    // 全局looger
     public static Logger logger;
     // 计分板对象
     public static Scoreboard scoreboard;
     // 全局配置实例
     public static Config config;
-    // 当前显示的计分板类型
-    public static boolean isShowingMining = true;
 }
