adccf14037ead25d1633c59c5eab8ad9a3dd36d8
Hycer <2765543491@qq.com>
2026-04-29 18:52:36 +0800

    fix:（AI）修复服务器重启后，离线玩家的数据不会显示的问题

--- src/main/java/cn/hycer/advancedscoreboard/Event/PlayerBreakBlockEvent.java
+++ src/main/java/cn/hycer/advancedscoreboard/Event/PlayerBreakBlockEvent.java
@@ -6,6 +6,9 @@ import cn.hycer.advancedscoreboard.Config.Config;
 import cn.hycer.advancedscoreboard.Config.ScoreboardItem;
 import cn.hycer.advancedscoreboard.Global.Global;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.scoreboard.ScoreAccess;
+import net.minecraft.scoreboard.ScoreHolder;
+import net.minecraft.scoreboard.ScoreboardObjective;
 
 public class PlayerBreakBlockEvent {
 
@@ -20,6 +23,28 @@ public class PlayerBreakBlockEvent {
         int playerScore = mineCountScoreboard.getDataValue(playerName, 0);
         //更新玩家的分数
         Global.config.getScoreboardByInternalName(Config.MINE_COUNT_INTERNAL_NAME).updateData(playerName, ++playerScore);
+        
+        // 同步到游戏内计分板
+        syncToScoreboard(playerName, playerScore);
+        
         logger.debug(Global.config.toString());
+    }
+    
+    /**
+     * 同步玩家挖掘数据到游戏内计分板
+     */
+    private static void syncToScoreboard(String playerName, int playerScore) {
+        try {
+            // 获取计分板对象
+            ScoreboardObjective objective = Global.scoreboard.getNullableObjective(internalName);
+            if (objective != null) {
+                // 更新游戏内计分板
+                ScoreHolder scoreHolder = ScoreHolder.fromName(playerName);
+                ScoreAccess scoreAccess = Global.scoreboard.getOrCreateScore(scoreHolder, objective);
+                scoreAccess.setScore(playerScore);
+            }
+        } catch (Exception e) {
+            logger.error("Failed to sync player data to scoreboard: {}", e.getMessage());
+        }
     }
 }
